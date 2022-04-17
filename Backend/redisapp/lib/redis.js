2b7a257e14c680e81e2952f512d2736a3dae6293
RichLeonC <r29leonc@gmail.com>
2022-04-16 18:54:18 -0600

    Update redis.js

--- Backend/redisapp/lib/redis.js
+++ Backend/redisapp/lib/redis.js
@@ -1,15 +1,15 @@
 import { Client, Entity, Schema, Repository } from "redis-om";
 
-const client= new Client();
+const client= new Client(); //Creamos un cliente para redis
 
-async function connect (){
+async function connect (){  //Conectamos con redis
     if (!client.isOpen){
-        await client.open(process.env.REDIS_URL);
+        await client.open(process.env.REDIS_URL); //Usamos la URL de la conexion
     }
 }
 
-class Car extends Entity{}
-let schema = new Schema(
+class Car extends Entity{} //Creamos la entidad Car
+let schema = new Schema( //Estructura del carro
     Car, {
         make: {type: 'string' },
         model: {type: 'string' },
@@ -24,11 +24,11 @@ let schema = new Schema(
 
 );
 
-export async function createCar(data){
-    await connect();
+export async function createCar(data){  //Creaction del carro
+    await connect(); //Conexion a REDIS
 
-    const repository = new Repository(schema, client);
-    const car = repository.createEntity(data);
-    const id = await repository.save(car);
-    return id;
+    const repository = new Repository(schema, client); //Guardamos el schema en el redis
+    const car = repository.createEntity(data); //Creamos una entidad del carro
+    const id = await repository.save(car); //guardamos el car
+    return id; //retornamos el id del car
 }
