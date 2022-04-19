f608913c93101dc5c2df5d89699c1d53a191f0b9
Adrián Herrera <91228585+AtlasHS09@users.noreply.github.com>
2022-04-18 20:01:39 -0600

    CRUD Documentado

--- Backend/redisapp/lib/CarForm.js
+++ Backend/redisapp/lib/CarForm.js
@@ -1,41 +1,38 @@
-import React from 'react';
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+export default function CarForm()  { // Función que genera un automovil en la base de datos
 
-export default function CarForm()  {
-
-    const handleSubmit = async (event) => {
-        event.preventDefault();
+    const handleSubmit = async (event) => { //Constante que envía el objeto al archivo cars.js
+        event.preventDefault(); //Previene errores que puedan surgir en el proceso de enviado
     
-        const form = new FormData(event.target);
-        const formData = Object.fromEntries(form.entries());
+        const form = new FormData(event.target); //Transforma los inputs del usuario en un Form
+        const formData = Object.fromEntries(form.entries()); //Convierte el Form previamente creado en un objeto listo para enviarse
     
-        const res = await fetch('/api/cars', {
-          body: JSON.stringify(formData),
+        const res = await fetch('/api/cars', { //Proceso de enviado a cars.js
+          body: JSON.stringify(formData), //Se envía en objeto en formato JSON
           headers: {
             'Content-Type': 'application/json',
           },
           method: 'POST',
         });
     
-        const result = await res.json();
-        console.log(result)
+        const result = await res.json(); //Se guarda el resultado del envío
+        console.log(result) //Se imprime en consola
       };
 
       return (
         <form onSubmit={handleSubmit}>
         <h3>Marca</h3>
-          <input name="make" type="text"  />
+          <input name="make" type="text"  /* Introduce la marca del vehículo*//> 
           <br></br>
         <h3>Modelo</h3>
-          <input name="model" type="text"  />
+          <input name="model" type="text" /* Introduce el modelo del vehículo*/ />
           <br></br>
         <h3>Imagen</h3>
-          <input name="image" type="text"  />
+          <input name="image" type="text"  /* Introduce una imagen del vehículo*/ />
           <br></br>
         <h3>Descripción</h3>
-          <textarea name="description" type="text"  />
+          <textarea name="description" type="text"  /* Introduce una descripción del vehículo*/ />
           <br></br>
-          <button type="submit">Ingresar Automovil</button>
+          <button type="submit">Ingresar Automovil </button> 
         </form>
       );
 }
--- Backend/redisapp/pages/api/cars.js
+++ Backend/redisapp/pages/api/cars.js
@@ -1,6 +1,6 @@
 import { createCar } from '../../lib/redis';
 
-export default async function handler(req, res) {
-    const id = await createCar(req.body);
-    res.status(200).json({ id })
-}
+export default async function handler(req, res) { //Función que recibe una petición y envía una respuesta
+    const id = await createCar(req.body); //Recibe el objeto que se creó en el archivo CarForm
+    res.status(200).json({ id }) //Envía como respuesta el ID del objeto enviado a la base de datos
+} 
