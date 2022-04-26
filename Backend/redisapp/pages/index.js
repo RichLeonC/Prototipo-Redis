6872ba181834748c5a6575f474b731af3dca5f66
RichLeonC <r29leonc@gmail.com>
2022-04-25 23:01:15 -0600

    Update

--- Backend/redisapp/pages/index.js
+++ Backend/redisapp/pages/index.js
@@ -1,11 +1,11 @@
+import { Search } from "redis-om";
 import CarForm from "../lib/CarForm";
 
 export default function Home() {
   return (
     <div>
-      <h1>Ingrese un Vehículo</h1>
-      <CarForm />
-      
+     <h1>Ingrese un Vehículo</h1>
+      <CarForm /> 
     </div>
   )
     
