0d91bbf4450e0569bc631f39754c2fd264caeed2
RichLeonC <r29leonc@gmail.com>
2022-04-27 18:12:36 -0600

    Bootstrap works

--- Backend/redisapp/lib/SearchForm.js
+++ Backend/redisapp/lib/SearchForm.js
@@ -1,27 +1,29 @@
 import { useState } from "react";
 
 export default function CarForm() {
-  
+
   const [hits, setHits] = useState([]);
 
   const search = async (event) => {
-    
+
     const q = event.target.value;
 
     if (q.length > 2) {
       const params = new URLSearchParams({ q })
-      console.log("q: "+q);
+      console.log("q: " + q);
       const res = await fetch('/api/search?' + params);
-      
+
       const result = await res.json();
       console.log(result)
       setHits(result['cars']);
     }
   }
 
-  return (<div>
-    <input onChange={search} type="text" />
-    
+  return (
+  <div className="container">
+
+    <h1>Busca tu carro favorito</h1>
+    <input className="form-control" onChange={search} type="text" />
     <ul>
       {hits.map((hit) => (
         <li key={hit.entityId}>
